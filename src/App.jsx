9f221fdd8aade6de082a2b654fe85da7a857d8b2
alaedin <mona.alaedin1999@gmail.com>
2024-09-25 17:41:32 +0330

    start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/auth" element={ } />
+    </Routes>
+  );
+}
+
+export default App;
